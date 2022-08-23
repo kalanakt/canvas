2a2d14e4111c93737d6822cf8ef9aa9cd4081c79
kalanakt <e19198@eng.pdn.ac.lk>
2022-08-24 03:36:09 +0530

    canvas [ 1 To 3 ]

---
+++ app/canvas.js
@@ -0,0 +1,7 @@
+var canvas = document.querySelector("canvas")
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+var c = canvas.getContext('2d');
+c.fillRect(100, 100, 100, 100)
